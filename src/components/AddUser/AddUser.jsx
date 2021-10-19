8d47f37a504768fec35575cbb6fe12dc2664d696
Shifat Reza <shifatreza44@gmail.com>
2021-10-19 17:43:41 +0600

    data filter out done

--- src/components/AddUser/AddUser.jsx
+++ src/components/AddUser/AddUser.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
-
+import { Country, State, City } from 'country-state-city';
 const AddUser = () => {
+    const states = State.getStatesOfCountry("BD");
+    // console.log(states);
+    // iso code is the state code
+
+    // const divFilter = states.filter((state) => state.name.includes('Division'));
+    // console.log(divFilter);
+
+    const divisions = City.getCitiesOfCountry("BD");
+    // console.log(divisions);
+    const divisionsFilter = divisions.filter(function (v, i) {
+        return (v["name"] === "Dhaka" || v["name"] === "Chittagong" || v["name"] === "Barisal" || v["name"] === "Khulna" || v["name"] === "Sylhet" || v["name"] === "Rajshahi");
+    });
+    console.log(divisionsFilter);
+
+    const allCities = City.getCitiesOfState("BD", "13");
+    console.log(allCities);
+
+    // states.forEach((state) => {
+    //     const stateName = state.name;
+    //     console.log(stateName);
+    //     const divFilter = stateName.filter()
+
+    // });
+    // const allStates = State.getAllStates();
+    // console.log(allStates);
+    // allStates.map((state) => console.log(state.name));
+    // console.log(Country.getAllCountries());
+    // console.log(State.getAllStates()[0]);
     return (
         <div>
             <h1>This is modal for add user</h1>
